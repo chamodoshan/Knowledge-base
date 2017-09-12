306693067bcacc2c483aa72ffa9e6f8734d402bf
chamodoshan <chamodoshan74@gmail.com>
2017-09-12 09:40:50 +0530

    update connection.php

--- test/connection.php
+++ test/connection.php
@@ -1,13 +1,22 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "knowledgebased";
+/**
+* 
+*/
+class connection
+{
+	public $servername = "localhost";
+	public $username = "root";
+	public $password = "";
+	public $dbname = "data set";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+	// Create connection
+	function check(){
+		$conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+		if ($conn->connect_error) {
+    		die("Connection failed: " . $conn->connect_error);
+		} 
+		return $conn;
+	}
+}
+
 ?>
